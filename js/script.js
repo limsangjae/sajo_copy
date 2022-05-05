95cd0944e38a6b380d541601e935b5a922cbec39
limsangjae <dlatkdwoa@gmail.com>
2022-05-05 23:37:33 +0900

    슬라이드 만드는중

--- js/script.js
+++ js/script.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
     $(".all-menu-box").click(function(){
         $(".nav-menu-wrap").toggleClass("active")
     })
+    $(".slider").slick({
+        arrows:false,
 
+    })
+    $(".left-btn").click(function () {
+        $(".slider").slick("slickPrev");
+    });
+    $(".right-btn").click(function () {
+        $(".slider").slick("slickNext");
+    });
 
     // 끝
 })
