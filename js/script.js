774b0553abed0a8fa68bb353ac907ef589aad37c
limsangjae <dlatkdwoa@gmail.com>
2022-05-06 21:47:55 +0900

    퀵메뉴 만드는중

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     })
     $(".slider").slick({
         arrows:false,
-
+        autoplay:true,
     })
     $(".left-btn").click(function () {
         $(".slider").slick("slickPrev");
@@ -12,6 +12,39 @@ $(document).ready(function(){
     $(".right-btn").click(function () {
         $(".slider").slick("slickNext");
     });
+    
+    $(".play").click(function(){
+        $(".play").removeClass("active");
+        $(".pause").addClass("active");
+        $(".slider").slick("slickPlay");
+    })
+    $(".pause").click(function(){
+        $(".play").addClass("active");
+        $(".pause").removeClass("active");
+        $(".slider").slick("slickPause");
+    })
+    $(".dots>.dot").click(function(){
+        let num = $(this).index();
+
+        $(".slider").slick("slickGoTo", num);
+        $(".dots>.dot").eq(num).addClass("active");
+        $(".dots>.dot").eq(num).siblings().removeClass("active");
+    })
+
+    $(".slider").on(
+        "beforeChange",
+        function (event, slick, currentSlide, nextSlide) {
+          console.log("비포체인지");
+          $(".dots>.dot").eq(nextSlide).addClass("active");
+          $(".dots>.dot").eq(nextSlide).siblings().removeClass("active");
+          
+          $(".content").removeClass("active")
+        }
+      );
+      $(".slider").on("afterChange", function (event, slick, currentSlide) {
+        console.log("에프터체인지");
+        $(".content").eq(currentSlide+1).addClass("active");
+      });
 
     // 끝
 })
